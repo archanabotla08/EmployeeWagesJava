9f9ec23cc04afbf0fd3e0c67c6b1bbbb91bc1c83
Nisha271 <bukkenisha@gmail.com>
2020-10-21 23:25:19 +0530

    [Archana] Added: UC4 with Employee Wage Part or Full Time using Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,18 +3,22 @@ import java.util.Random;
 		public static void main(String[] args){
 			//System.out.println("Welcome to Master");
 			Random random = new Random();
-                	int upperbond = 2;
+                	int upperbond = 3;
                 	int randomNumber = random.nextInt(upperbond);
 			int empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1;
-                	if(randomNumber == isFullTime) {
-                 		empHr = 8;
-			}else if(randomNumber == isPartTime) {
+                	switch(randomNumber){
+                		case 2:
+                        	empHr = 8;
+                        	break;
+                		case 1:
                         	empHr = 4;
-                	}else {
-                        	empHr = 0;
-                	}
+                        	break;
+                		default:
+                        	empHr =0;
+                       	 	break;
+                }
 				int Salary = empRatePerHr * empHr;
-                		System.out.println("Employee Daily Wage Based on Part of full Time :");
+                		System.out.println("Employee Daily Wage Based on Part of full Time Case :");
                 		System.out.println("Salary = " + Salary);
 		}
 	}
