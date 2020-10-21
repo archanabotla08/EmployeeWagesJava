243500821d3d8d6df6fcc3847c594d16b69661cb
Nisha271 <bukkenisha@gmail.com>
2020-10-21 23:35:44 +0530

    [Archana] Added: UC5 with Employee Wage for Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,22 +3,24 @@ import java.util.Random;
 		public static void main(String[] args){
 			//System.out.println("Welcome to Master");
 			Random random = new Random();
-                	int upperbond = 3;
-                	int randomNumber = random.nextInt(upperbond);
-			int empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1;
-                	switch(randomNumber){
-                		case 2:
-                        	empHr = 8;
-                        	break;
-                		case 1:
-                        	empHr = 4;
-                        	break;
-                		default:
-                        	empHr =0;
-                       	 	break;
-                }
+			int upperbond = 3 , empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1,numWorkingDays = 20,totalSalary = 0;
+                	for(int day=1;day<=numWorkingDays;day++) {
+                 		int randomNumber = random.nextInt(upperbond);
+				switch(randomNumber){
+                			case 2:
+                        		empHr = 8;
+                        		break;
+                			case 1:
+                        		empHr = 4;
+                        		break;
+                			default:
+                        		empHr =0;
+                       	 		break;
+                	}
 				int Salary = empRatePerHr * empHr;
-                		System.out.println("Employee Daily Wage Based on Part of full Time Case :");
-                		System.out.println("Salary = " + Salary);
+				totalSalary = totalSalary + Salary;
+		}
+                		System.out.println("Employee Wage Month :");
+                		System.out.println("Total Salary = " + totalSalary);
 		}
 	}
