9c4dcfdbdc395edd5aa17ab9a7d5653ed297f893
Nisha271 <bukkenisha@gmail.com>
2020-10-21 23:49:34 +0530

    [Archana] Added: UC6 with EmployeeWage for Max_Hrs_in_Month and total Working days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,8 +3,8 @@ import java.util.Random;
 		public static void main(String[] args){
 			//System.out.println("Welcome to Master");
 			Random random = new Random();
-			int upperbond = 3 , empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1,numWorkingDays = 20,totalSalary = 0;
-                	for(int day=1;day<=numWorkingDays;day++) {
+			int upperbond = 3 , empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1,numWorkingDays = 20,totalSalary = 0,MAX_HRS_IN_MONTH = 100,totalWorkingDays = 0, totalEmpHrs=0;
+                	while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < numWorkingDays ) {
                  		int randomNumber = random.nextInt(upperbond);
 				switch(randomNumber){
                 			case 2:
@@ -17,10 +17,10 @@ import java.util.Random;
                         		empHr =0;
                        	 		break;
                 	}
-				int Salary = empRatePerHr * empHr;
-				totalSalary = totalSalary + Salary;
+				totalEmpHrs = totalEmpHrs + empHr;
 		}
-                		System.out.println("Employee Wage Month :");
+				totalSalary = totalEmpHrs * empRatePerHr;
+                		System.out.println("Employee Wage Month With MAX_HRS_IN_MONTH::");
                 		System.out.println("Total Salary = " + totalSalary);
 		}
 	}
