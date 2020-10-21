a8515af74d13906e49c03f2019a026bd0f35fa8f
Nisha271 <bukkenisha@gmail.com>
2020-10-21 22:44:44 +0530

    [Archana] Added: code for Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,14 @@
-public class EmployeeWage{
+import java.util.Random;
+	public class EmployeeWage{
 		public static void main(String[] args){
-			System.out.println("Welcome to Master");
+			//System.out.println("Welcome to Master");
+			Random random = new Random();
+                	int upperbond = 2;
+                	int randomNumber = random.nextInt(upperbond);
+                	if(randomNumber == 1) {
+                        	System.out.println("Employee is present");
+                	}else {
+                        	System.out.println("Employee is absent");
+                	}
 		}
 	}
