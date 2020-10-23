0e837c79ed8b8693591cb9932c262ddd33059858
Nisha271 <bukkenisha@gmail.com>
2020-10-23 07:27:34 +0530

    [Archana] Refactored: Added UC7 with all code refactored for cllass

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,44 @@
 import java.util.Random;
 	public class EmployeeWage{
+		private final int NUMBER_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
+		int totalWorkingDays = 0,empRatePerHr = 20, empHr ,randomNumber,totalEmpHr,totalSalary;
+		public int randomGeneration() {
+  				Random random = new Random();
+    				int upperbond = 3;
+			    	randomNumber = random.nextInt(upperbond);
+    				return randomNumber;
+ 		}
+		public int calculateEmpHr() {
+  			while (totalEmpHr < MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_WORKING_DAYS) 
+			{
+  				int randomNumber = randomGeneration();
+     				switch(randomNumber){
+     				case 2:
+        				empHr = 8;
+        			break;
+     				case 1:
+        				empHr = 4;
+        			break;
+     				default:
+        				empHr =0;
+        			break;
+    				}
+     				totalEmpHr = totalEmpHr + empHr;
+    			}
+    			return totalEmpHr;
+ 		}
+		public int totalSalary() {
+  			totalSalary = totalEmpHr * empRatePerHr ;
+     			return totalSalary;
+ 		}
+ 		public void display(int totalSalary) {
+      			System.out.println("Salary = "+ totalSalary);
+ 		}
 		public static void main(String[] args){
-			//System.out.println("Welcome to Master");
-			Random random = new Random();
-			int upperbond = 3 , empRatePerHr = 20,empHr = 0,isFullTime = 2,isPartTime = 1,numWorkingDays = 20,totalSalary = 0,MAX_HRS_IN_MONTH = 100,totalWorkingDays = 0, totalEmpHrs=0;
-                	while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < numWorkingDays ) {
-                 		int randomNumber = random.nextInt(upperbond);
-				switch(randomNumber){
-                			case 2:
-                        		empHr = 8;
-                        		break;
-                			case 1:
-                        		empHr = 4;
-                        		break;
-                			default:
-                        		empHr =0;
-                       	 		break;
-                	}
-				totalEmpHrs = totalEmpHrs + empHr;
-		}
-				totalSalary = totalEmpHrs * empRatePerHr;
-                		System.out.println("Employee Wage Month With MAX_HRS_IN_MONTH::");
-                		System.out.println("Total Salary = " + totalSalary);
-		}
+			EmployeeWage emp = new EmployeeWage();
+                	emp.randomGeneration();
+                	emp.calculateEmpHr();
+                	int salary = emp.totalSalary();
+                	emp.display(salary);
 	}
+}
