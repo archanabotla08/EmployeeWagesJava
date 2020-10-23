c624c3beacd6be4551736f673f43fe635b4b22b6
Nisha271 <bukkenisha@gmail.com>
2020-10-23 07:43:01 +0530

    [Archana] Added: UC8 with employee wage by passing parameter functions excluding class variables

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 	public class EmployeeWage{
-		private final int NUMBER_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
-		int totalWorkingDays = 0,empRatePerHr = 20, empHr ,randomNumber,totalEmpHr,totalSalary;
+		//private final int NUMBER_WORKING_DAYS = 20, MAX_HRS_IN_MONTH = 100;
+		//int totalWorkingDays = 0,empRatePerHr = 20, empHr ,randomNumber,totalEmpHr,totalSalary;
 		public int randomGeneration() {
   				Random random = new Random();
     				int upperbond = 3;
-			    	randomNumber = random.nextInt(upperbond);
+				int randomNumber = random.nextInt(upperbond);
     				return randomNumber;
  		}
-		public int calculateEmpHr() {
-  			while (totalEmpHr < MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_WORKING_DAYS) 
+		public void calculateEmpHr(String name,int MAX_HRS_IN_MONTH,int TOTAL_WORKING_DAYS) {
+			  	int max =  MAX_HRS_IN_MONTH;
+    			  	int Days = TOTAL_WORKING_DAYS;
+    			  	String companyName = name;
+    				int empRatePerHr =20,empHr=0,totalWorkingDays=0,totalSalary=0,totalEmpHr=0;
+  			while (totalEmpHr < MAX_HRS_IN_MONTH && totalWorkingDays < Days) 
 			{
   				int randomNumber = randomGeneration();
      				switch(randomNumber){
@@ -25,20 +30,22 @@ import java.util.Random;
     				}
      				totalEmpHr = totalEmpHr + empHr;
     			}
-    			return totalEmpHr;
- 		}
-		public int totalSalary() {
-  			totalSalary = totalEmpHr * empRatePerHr ;
-     			return totalSalary;
- 		}
- 		public void display(int totalSalary) {
-      			System.out.println("Salary = "+ totalSalary);
+    			totalSalary = totalEmpHr * empRatePerHr ;
+     			System.out.println("Employee name = "+companyName+" "+"Employee Salary = "+totalSalary);
  		}
 		public static void main(String[] args){
 			EmployeeWage emp = new EmployeeWage();
-                	emp.randomGeneration();
-                	emp.calculateEmpHr();
-                	int salary = emp.totalSalary();
-                	emp.display(salary);
+                	Scanner sc =  new Scanner(System.in);
+                	System.out.println("Enter the number of company's");
+                	int companyNumber = sc.nextInt();
+                	for(int i=1;i<=companyNumber;i++){
+                	System.out.println("Enter the company's name");
+                 	String name = sc.next();
+                	System.out.println("Enter the MAX_HRS_IN_MONTH");
+                	int MAX_HRS_IN_MONTH = sc.nextInt();
+                	System.out.println("Enter the working days");
+                	int TOTAL_WORKING_DAYS = sc.nextInt();
+                	emp.calculateEmpHr(name,MAX_HRS_IN_MONTH,TOTAL_WORKING_DAYS);
+			}
+		}
 	}
-}
